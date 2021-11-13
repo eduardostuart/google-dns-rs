8094ff695fe1e7f41322027a6cf9b1938b9721fe
Eduardo Stuart <hi@s.tuart.me>
2021-11-13 14:19:15 +0100

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
-///! # google-dns-rs
-///! google-dns-rs is a third party Google DNS client for rust.
-///!
-///! ```no_run
-///! use google_dns_rs::api::{DoH, Dns, Result};
-///! pub async fn resolve_github() -> Result<Dns> {
-///!    // DNS Over HTTPS
-///!    Ok(DoH::new("github.com".to_string()).resolve().await?)
-///! }
-///! ```
+//! # google-dns-rs
+//! google-dns-rs is a third party Google DNS client for rust.
+//!
+//! ```no_run
+//! use google_dns_rs::api::{Dns, DoH, Result};
+//! async fn github_dns_records() -> Result<Dns> {
+//!     DoH::new("github.com".to_string())
+//!         // .set_type(2) // NS record type
+//!         // .set_cd(true) // disable or enable DNSSEC check
+//!         // .set_ct("application/x-javascript".to_string()) // content type
+//!         // .set_do(true) // include DNSSEC recods
+//!         .resolve()
+//!         .await
+//! }
+//! ```
+
 pub mod api;
 pub mod error;
